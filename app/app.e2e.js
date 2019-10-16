bb0a5d191723815354a61167e87360260fcfcffa
Ben Monro <benjamin.monro@walmartlabs.com>
2019-10-15 21:11:48 -0700

    add example test for react-boilerplate

--- app/app.e2e.js
+++ app/app.e2e.js
@@ -1,8 +1,12 @@
 /* eslint-disable */
 import { getByText } from '@testing-library/testcafe';
 
-fixture('Testing testcafe-testing-library').page('/');
+fixture('Testing testcafe-testing-library').page('http://localhost:3000');
 
-test('it finds something by text', async () => {
-  getByText('');
+test('it finds something by text', async (t) => {
+  const featuresButton = getByText('Features');
+
+  await t
+    .click(featuresButton)
+    .expect(getByText("Quick scaffolding").exists).ok()
 });
